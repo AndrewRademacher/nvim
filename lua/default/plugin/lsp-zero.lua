f4176e76c5bcaa7f8905057adca337ab1cd30776
AndrewRademacher <andrewrademacher@icloud.com>
2024-01-21 10:25:18 -0600

    Fix ensure lsp for languages.

--- lua/default/plugin/lsp-zero.lua
+++ lua/default/plugin/lsp-zero.lua
@@ -7,29 +7,6 @@ return {
 			vim.g.lsp_zero_extend_lspconfig = 0
 		end,
 		config = false,
-		--		config = function()
-		--			local lsp = require("lsp-zero").preset({})
-		--
-		--			lsp.on_attach(function(client, bufnr)
-		--				-- see :help lsp-zero-keybindings
-		--				-- to learn the available actions
-		--				lsp.default_keymaps({ buffer = bufnr })
-		--			end)
-		--
-		--			--lsp.ensure_installed({
-		--			--	"jedi_language_server",
-		--			--	"julials",
-		--			--	"lua_ls",
-		--			--	"rust_analyzer",
-		--			--	"texlab",
-		--			--	"zls",
-		--			--})
-		--
-		--			-- (Optional) Configure lua language server for neovim
-		--			require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
-		--
-		--			lsp.setup()
-		--		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
@@ -51,7 +28,14 @@ return {
 			end)
 
 			require("mason-lspconfig").setup({
-				ensure_installed = {},
+				ensure_installed = {
+					"pylyzer",
+					"julials",
+					"lua_ls",
+					"rust_analyzer",
+					"texlab",
+					"zls",
+				},
 				handlers = {
 					lsp_zero.default_setup,
 					lua_ls = function()
@@ -71,7 +55,6 @@ return {
 			lsp_zero.extend_cmp()
 
 			local cmp = require("cmp")
-			-- local cmp_action = lsp_zero.cmp_action()
 
 			local cmp_select = { behavior = cmp.SelectBehavior.Select }
 			cmp.setup({
@@ -83,14 +66,6 @@ return {
 					["<C-Space>"] = cmp.mapping.complete(),
 				}),
 			})
-
-			--    local cmp_select = { behavior = cmp.SelectBehavior.Select }
-			--	local cmp_mappings = lsp.defaults.cmp_mappings({
-			--		["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-			--		["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-			--		["<C-y>"] = cmp.mapping.confirm({ select = true }),
-			--		["<C-Space>"] = cmp.mapping.complete(),
-			--	})
 		end,
 	},
 	{ "L3MON4D3/LuaSnip" },
