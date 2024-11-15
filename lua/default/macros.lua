5d5dc059ce5fab1acc05e49eaf07053ecec6a0e1
AndrewRademacher <andrewrademacher@icloud.com>
2024-11-15 05:32:43 -0600

    Macros.

--- lua/default/macros.lua
+++ lua/default/macros.lua
@@ -1,9 +1,18 @@
-vim.api.nvim_create_user_command("Test", "echo 'It Works'", {})
-
-vim.api.nvim_create_user_command("HLine", function(opts)
+vim.api.nvim_create_user_command("HLine", function()
 	local line = "// "
 	line = line .. string.rep("-", 120 - string.len(line))
 
 	local cmd = "i" .. line .. "<ESC>j"
 	vim.api.nvim_input(cmd)
 end, {})
+
+vim.api.nvim_create_user_command("Section", function(opts)
+	local line = "// "
+	line = line .. string.rep("-", 120 - string.len(line)) .. "\n"
+
+	local label = "// "
+	label = label .. opts.fargs[1] .. "\n"
+
+	local cmd = "i" .. line .. label .. line .. "<ESC>j"
+	vim.api.nvim_input(cmd)
+end, { nargs = 1 })
