40815a168094eecb1b2bcdac0efa98e4cf826c3a
AndrewRademacher <andrewrademacher@icloud.com>
2024-11-15 05:36:38 -0600

    Remove macro.

--- lua/default/macros.lua
+++ lua/default/macros.lua
@@ -5,14 +5,3 @@ vim.api.nvim_create_user_command("HLine", function()
 	local cmd = "i" .. line .. "<ESC>j"
 	vim.api.nvim_input(cmd)
 end, {})
-
-vim.api.nvim_create_user_command("Section", function(opts)
-	local line = "// "
-	line = line .. string.rep("-", 120 - string.len(line)) .. "\n"
-
-	local label = "// "
-	label = label .. opts.fargs[1] .. "\n"
-
-	local cmd = "i" .. line .. label .. line .. "<ESC>j"
-	vim.api.nvim_input(cmd)
-end, { nargs = 1 })
